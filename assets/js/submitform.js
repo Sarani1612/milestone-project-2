bd423a0ee6b0386d06b83c34a4c6cd4799166b22
Sarani1612 <sara.larsen1612@gmail.com>
2019-11-09 15:10:51 +0000

    added function for submitting form

--- assets/js/submitform.js
+++ assets/js/submitform.js
@@ -1,1 +1,16 @@
-console.log("Hello!");
+function submitForm(contactForm) {
+    emailjs.send("default_service", "theatre_travels", {
+        "fullname": contactForm.name.value,
+        "emailaddress": contactForm.emailaddress.value,
+        "comments": contactForm.comments.value,
+        "dates": contactForm.dates.value,
+        "nights": contactForm.nights.value
+    })
+    .then(
+        function(response) {
+            console.log("Success!", response);
+        },
+        function(error) {
+            console.log("Failed", error);
+        });
+}
