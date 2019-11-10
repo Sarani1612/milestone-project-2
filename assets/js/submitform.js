2c45c68d1be14389852574ce7cc5a97830e3053c
Sarani1612 <sara.larsen1612@gmail.com>
2019-11-10 15:28:02 +0000

    fixed error in styling the form button on submit

--- assets/js/submitform.js
+++ assets/js/submitform.js
@@ -11,7 +11,8 @@ function submitForm(contactForm) {
     emailjs.send("gmail", "theatre_travels", formDetails)
     .then(
         function(response) {
-            document.querySelector("button").innerHTML="Sent!".classList.replace("form-button", "form-button-sent");
+            document.querySelector("button").innerHTML="Sent!";
+            document.querySelector("button").classList.replace("form-button", "form-button-sent");
             alert("Success!", response);
             document.getElementById("contact-form").reset();
             console.log("Success!", response);
