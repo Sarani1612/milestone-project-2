049d8ab03ded9bae7abe6a603c7564cf674e7460
Sarani1612 <sara.larsen1612@gmail.com>
2019-11-10 13:06:19 +0000

    created variable for form details

--- assets/js/submitform.js
+++ assets/js/submitform.js
@@ -1,11 +1,14 @@
 function submitForm(contactForm) {
-    emailjs.send("gmail", "theatre_travels", {
+
+    var formDetails = {
         "fullname": contactForm.name.value,
         "emailaddress": contactForm.emailaddress.value,
         "comments": contactForm.comments.value,
         "dates": contactForm.dates.value,
         "nights": contactForm.nights.value
-    })
+    };
+
+    emailjs.send("gmail", "theatre_travels", formDetails)
     .then(
         function(response) {
             console.log("Success!", response);
