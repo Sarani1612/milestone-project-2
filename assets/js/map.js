7de686f496b1d843dbbaceb0257af0227701a5e5
Sarani1612 <sara.larsen1612@gmail.com>
2019-11-24 12:53:03 +0000

    added map for London

--- assets/js/map.js
+++ assets/js/map.js
@@ -24,20 +24,33 @@ window.onload = function(){
 };
 
 // Initializing map
+function initMap() {
+    // Location of places in Paris
+    var senat = {lat: 48.849111, lng: 2.339784};
+    var epidon = {lat: 48.850865, lng: 2.339383};
+    var odeon = {lat: 48.849493, lng: 2.338734};
 
-    function initMap() {
-        // Location of places in Paris
-        var senat = {lat: 48.849, lng: 2.339};
-        var epidon = {lat: 48.850, lng: 2.339};
-        var odeon = {lat: 48.849493, lng: 2.338734};
-        // Map centered on hotel
-        var map = new google.maps.Map(
-            document.getElementById('map-paris'), {zoom: 15, center: senat});
-            // Markers for hotel, restaurant and theatre
-            var marker = new google.maps.Marker({position: senat, map: map});
-            var marker2 = new google.maps.Marker({position: epidon, map: map});
-            var marker3 = new google.maps.Marker({position: odeon, map: map});
-    }
+    // Location of places in London
+    var courthouse = {lat: 51.514351, lng: -0.139485};
+    var mayfair = {lat: 51.512698, lng: -0.145551};
+    var palladium = {lat: 51.514600, lng: -0.140465};
 
+    // Map centered on hotel in Paris
+    var map = new google.maps.Map(
+        document.getElementById('map-paris'), {zoom: 15, center: senat});
+        // Markers for hotel, restaurant and theatre
+        var marker0 = new google.maps.Marker({position: senat, map: map});
+        var marker1 = new google.maps.Marker({position: epidon, map: map});
+        var marker2 = new google.maps.Marker({position: odeon, map: map});
 
+    // Map centered on hotel in London
+    var mapLondon = new google.maps.Map(
+        document.getElementById('map-london'), {zoom: 15, center: courthouse});
+        // Markers for hotel, restaurant and theatre
+        var marker0 = new google.maps.Marker({position: courthouse, map: mapLondon});
+        var marker1 = new google.maps.Marker({position: mayfair, map: mapLondon});
+        var marker2 = new google.maps.Marker({position: palladium, map: mapLondon});
 
+}   
+
+    
