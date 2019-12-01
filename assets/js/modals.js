d6c3d4aa80bc130fc85a20ca9febf476d04831f3
Sarani1612 <sara.larsen1612@gmail.com>
2019-12-01 17:05:54 +0000

    added function for closing modals with button

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -1,22 +1,30 @@
 window.onload = function(){ 
 
-//Loop for opening modals
-
+    // Opens modals
     var cityDiv = document.getElementsByClassName('city-div');
 
     for (var i = 0; i < cityDiv.length; i++) {
         var thisCityDiv = cityDiv[i];
-        thisCityDiv.addEventListener('click', function(){
+        thisCityDiv.onclick = function() {
             var modal = document.getElementById(this.dataset.modal);
             $(modal).show();
-        }, false);
+        }, false;
 
     }
 
-// event for closing modals when clicking outside of it
+    // Closes modals when clicking outside of it
 
     window.onclick = function(event) {
         if (event.target.className == 'modal') {
+            $('.modal').hide();
+        }
+    }
+
+    // Closes modals when clicking on close button
+
+    var closeModal = document.getElementsByClassName('modal-close');
+    for (var i = 0; i < closeModal.length; i++) {
+        closeModal[i].onclick = function() {
             $('.modal').hide();
         }
     }
