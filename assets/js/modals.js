2b727e05bdd4d666aa07b3976664c0d3057be8f2
Sarani1612 <sara.larsen1612@gmail.com>
2019-12-01 16:39:25 +0000

    moved JS for modals to its own separate file

---
+++ assets/js/modals.js
@@ -0,0 +1,24 @@
+window.onload = function(){ 
+
+//Loop for opening modals
+
+    var cityDiv = document.getElementsByClassName('city-div');
+
+    for (var i = 0; i < cityDiv.length; i++) {
+        var thisCityDiv = cityDiv[i];
+        thisCityDiv.addEventListener('click', function(){
+            var modal = document.getElementById(this.dataset.modal);
+            $(modal).show();
+        }, false);
+
+    }
+
+// event for closing modals when clicking outside of it
+
+    window.onclick = function(event) {
+        if (event.target.className == 'modal') {
+            $('.modal').hide();
+        }
+    }
+
+};
