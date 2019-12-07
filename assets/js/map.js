df66a56400796a0d09bf14fc902643d362f92b57
Sarani1612 <sara.larsen1612@gmail.com>
2019-12-07 18:24:35 +0000

    changed zoom lvl for maps to avoid some pins not showing on smaller screens

--- assets/js/map.js
+++ assets/js/map.js
@@ -13,60 +13,60 @@ function initMap() {
         {name: 'Courthouse Hotel', lat: 51.514351, lng: -0.139485},
         {name: 'Mew of Mayfair Brasserie', lat: 51.512698, lng: -0.145551},
         {name: 'London Palladium', lat: 51.514600, lng: -0.140465}
-    ]
+    ];
     // Location of places in Prague
     const prague = [
         {name: 'Hotel Leonardo', lat: 50.083632, lng: 14.414274},
         {name: 'Restaurant Parnas', lat: 50.081683, lng: 14.413252},
         {name: 'National Theatre', lat: 50.080980, lng: 14.413598}
-    ]
+    ];
     // Location of places in Vienaa
     const vienna = [
         {name: 'Hotel Josefshof am Rathaus', lat: 48.208602, lng: 16.354076},
         {name: 'Fromme Helene', lat: 48.209242, lng: 16.352121},
         {name: 'Vienna Operahouse', lat: 48.202961, lng: 16.368914}
-    ]
+    ];
     // Location of places in Athens
     const athens = [
         {name: 'Fresh Hotel', lat: 37.981244, lng: 23.726716},
         {name: 'Kuzina', lat: 37.976413, lng: 23.722061},
         {name: 'National Theatre of Athens', lat: 37.984969, lng: 23.725248}
-    ]
+    ];
     // Location of places in Rome
     const rome = [
         {name: 'Rome Times Hotel', lat: 41.898567, lng: 12.490433},
         {name: 'Il Nido dell Pettirosso', lat: 41.899409, lng: 12.493029},
         {name: "Teatro dell'Opera", lat: 41.900876, lng: 12.495336}
-    ]
+    ];
     // Location of places in Stockholm
     const stockholm = [
         {name: 'Nobis Hotel', lat: 59.333451, lng: 18.073844},
         {name: 'Restaurang Prinsen', lat: 59.334417, lng: 18.073255},
         {name: 'Royal Swedish Opera', lat: 59.329867, lng: 18.070386}
-    ]
+    ];
     // Location of places in Dublin
     const dublin = [
         {name: 'Brooks Hotel', lat: 53.341536, lng: -6.263617},
         {name: 'The Oak', lat: 53.344360, lng: -6.267133},
         {name: 'Olympia Theatre', lat: 53.344304, lng: -6.266082}
-    ]
+    ];
     // Location of places in Helsinki
     const helsinki = [
         {name: 'Clarion Hotel', lat: 60.159933, lng: 24.922068},
         {name: 'Restaurant Natura', lat: 60.163663, lng: 24.942200},
         {name: 'Alexander Theatre', lat: 60.163432, lng: 24.933357}
-    ]
+    ];
 
     // Map centered on hotels in each city
-    var mapParis = new google.maps.Map(document.getElementById('map-paris'), {zoom: 15, center: paris[0]});
-    var mapLondon = new google.maps.Map(document.getElementById('map-london'), {zoom: 15, center: london[0]});
-    var mapPrague = new google.maps.Map(document.getElementById('map-prague'), {zoom: 15, center: prague[0]});
-    var mapVienna = new google.maps.Map(document.getElementById('map-vienna'), {zoom: 14, center: vienna[0]});
-    var mapAthens = new google.maps.Map(document.getElementById('map-athens'), {zoom: 15, center: athens[0]});
-    var mapRome = new google.maps.Map(document.getElementById('map-rome'), {zoom: 15, center: rome[0]});
-    var mapStockholm = new google.maps.Map(document.getElementById('map-stockholm'), {zoom: 15, center: stockholm[0]});
-    var mapDublin = new google.maps.Map(document.getElementById('map-dublin'), {zoom: 15, center: dublin[0]});
-    var mapHelsinki = new google.maps.Map(document.getElementById('map-helsinki'), {zoom: 14, center: helsinki[0]});
+    var mapParis = new google.maps.Map(document.getElementById('map-paris'), {zoom: 13, center: paris[0]});
+    var mapLondon = new google.maps.Map(document.getElementById('map-london'), {zoom: 13, center: london[0]});
+    var mapPrague = new google.maps.Map(document.getElementById('map-prague'), {zoom: 13, center: prague[0]});
+    var mapVienna = new google.maps.Map(document.getElementById('map-vienna'), {zoom: 13, center: vienna[0]});
+    var mapAthens = new google.maps.Map(document.getElementById('map-athens'), {zoom: 13, center: athens[0]});
+    var mapRome = new google.maps.Map(document.getElementById('map-rome'), {zoom: 13, center: rome[0]});
+    var mapStockholm = new google.maps.Map(document.getElementById('map-stockholm'), {zoom: 13, center: stockholm[0]});
+    var mapDublin = new google.maps.Map(document.getElementById('map-dublin'), {zoom: 13, center: dublin[0]});
+    var mapHelsinki = new google.maps.Map(document.getElementById('map-helsinki'), {zoom: 13, center: helsinki[0]});
 
     function mapMarkers(city, map) {
         for(var i = 0; i < city.length; i++){
@@ -77,8 +77,7 @@ function initMap() {
             animation: google.maps.Animation.DROP,
             title: pins.name
             });
-        }
-        
+        };
     };
         
     mapMarkers(paris, mapParis);
@@ -91,4 +90,4 @@ function initMap() {
     mapMarkers(dublin, mapDublin);
     mapMarkers(helsinki, mapHelsinki);
 
-}
+};
